6d8d23fba5e9f8e5a6742c0acda8c0d2d87da82c
pokuwagata <pokuwagata@gmail.com>
2020-02-10 00:23:13 +0900

    learn vue

--- doc/todo.md
+++ doc/todo.md
@@ -1,8 +1,17 @@
 # todo
 
-- [ ] nuxt.js入れる
-
+- [x] nuxt.js入れる
+- [x] netlifyにデプロイ
+- [x] vue.js復習
+- [ ] nuxt上でvue.jsだけで簡単に実装する（nuxtの詳細を知りたくないのでindexだけで何か動かす）
+- [ ] vuex学ぶ
+- [ ] vuexでリファクタする
 
 ## はまり
 
 - <https://stackoverflow.com/questions/6632191/how-to-revert-initial-git-commit>
+- vueコンポーネントとvueインスタンスは違う、vueコンポーネントはdataは関数にしないとthisで参照できない
+  - <https://jp.vuejs.org/v2/guide/single-file-components.html>
+  - <https://jp.vuejs.org/v2/guide/components.html>
+- テンプレート構文は省略記法がいくつかある
+  - <https://jp.vuejs.org/v2/guide/syntax.html>
--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,55 @@
 <template>
-  <h1>Hello world!</h1>
+  <div>
+    <h1>forum</h1>
+    <form v-on:submit.prevent="createThred">
+      <div>
+        <label for="thread-name">スレッド名</label>
+        <input type="text" id="thread-name" v-model="threadName" />
+      </div>
+      <div>
+        <label for="thread-content">内容</label>
+        <textarea id="thread-content" v-model="threadContent"></textarea>
+      </div>
+      <button>スレッド作成</button>
+    </form>
+    <h2>スレッド一覧</h2>
+    <div>
+      <ul>
+        <li v-for="thread in threads" v-on:click="selectThread(thread.id)">
+          <a href="" v-on:click.prevent>{{ thread.name }}</a>
+        </li>
+      </ul>
+    </div>
+    <div v-if="selectedThread">
+      {{ threads[selectedThread].content }}
+    </div>
+  </div>
 </template>
+
+<script>
+export default {
+  name: "Hoge",
+  data() {
+    return {
+      threadId: 0,
+      threadName: null,
+      threadContent: null,
+      threads: [],
+      selectedThread: null
+    };
+  },
+  methods: {
+    createThred: function() {
+      this.threads.push({
+        id: this.threadId,
+        name: this.threadName,
+        content: this.threadContent
+      });
+      this.threadId++;
+    },
+    selectThread: function(id) {
+      this.selectedThread = id;
+    }
+  }
+};
+</script>
