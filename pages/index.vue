4a5622249f2f3d3bf84c5aee7c8c0dde5dd4e7d3
pokuwagata <pokuwagata@gmail.com>
2020-02-10 21:29:33 +0900

    create init-version

--- pages/index.vue
+++ pages/index.vue
@@ -21,7 +21,22 @@
       </ul>
     </div>
     <div v-if="selectedThread">
-      {{ threads[selectedThread].content }}
+      <h3>{{ selectedThread.name }}</h3>
+      <p>{{ selectedThread.content }}</p>
+      <h4>コメント一覧</h4>
+      <ul v-if="selectedThread.comments.length > 0">
+        <li v-for="comment in selectedThread.comments">
+          {{ comment.user }} : {{ comment.content }}
+        </li>
+      </ul>
+      <form v-on:submit.prevent="createComment">
+        <div>
+          <label for="user-name">ユーザ名</label>
+          <input type="text" id="user-name" v-model="user"/>
+        </div>
+        <textarea id="user-content" v-model="commentContent"></textarea>
+        <button style="display: block;">書き込む</button>
+      </form>
     </div>
   </div>
 </template>
@@ -35,20 +50,37 @@ export default {
       threadName: null,
       threadContent: null,
       threads: [],
-      selectedThread: null
+      selectedThreadId: null,
+      commentId: 0,
+      user: null,
+      commentContent: null
     };
+  },
+  computed: {
+    selectedThread: function() {
+      return this.threads[this.selectedThreadId];
+    }
   },
   methods: {
     createThred: function() {
       this.threads.push({
         id: this.threadId,
         name: this.threadName,
-        content: this.threadContent
+        content: this.threadContent,
+        comments: []
       });
       this.threadId++;
     },
     selectThread: function(id) {
-      this.selectedThread = id;
+      this.selectedThreadId = id;
+    },
+    createComment: function() {
+      this.threads[this.selectedThreadId].comments.push({
+        id: this.commentId,
+        user: this.user,
+        content: this.commentContent
+      });
+      this.commentId++;
     }
   }
 };
